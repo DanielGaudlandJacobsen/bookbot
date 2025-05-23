2b4631ee6db094794593b07bee03aaa03def3e08
DanielGaudlandJacobsen <djacobsen5@gmail.com>
2025-05-23 16:08:55 +0200

    refactor

---
+++ stats.py
@@ -0,0 +1,34 @@
+def count_words(text):
+    words = text.split()
+    return len(words)
+
+
+def count_characters(text):
+    character_count = {}
+    for c in text.lower():
+        if c not in character_count:
+            character_count[c] = 1
+        else:
+            character_count[c] += 1
+    return character_count
+
+
+def sort_on(dict):
+    return dict["num"]
+
+
+def sorted_char_list(dict):
+    char_list = []
+    for k in dict:
+        if k.isalpha():
+            char_list.append({"char": k, "num": dict[k]})
+    char_list.sort(reverse=True, key=sort_on)
+    return char_list
+
+
+def print_report(path, int, lst):
+    print(f"--- Report of {path} ---")
+    print(f"{int} words found in the document.\n")
+    for dict in lst:
+        print(f"The {dict['char']} character was found {dict['num']} times")
+    print("--- End report ---")
